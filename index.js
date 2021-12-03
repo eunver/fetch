244cfde60bc28540de67aa5e3e25318e8daba152
eunver <iemreunver@gmail.com>
2021-12-03 17:03:16 -0500

    aa

--- index.js
+++ index.js
@@ -12,9 +12,15 @@ async function getDegrees(url) {
       .then((response) => response.json())
       .then((data) =>
         data.data.forEach((element) => {
+          if(element.degree.year>2021) {
           document.getElementById("degrees").innerHTML+=
           `I recieved my ${element.degree.type} degree in 
-          ${element.degree.program} from ${element.degree.school} in ${element.degree.year}`+ "<br />" ;
+          ${element.degree.program} from ${element.degree.school} in ${element.degree.year}`+ "<br />"}
+          else{
+            document.getElementById("degrees").innerHTML+=
+            `I will recieve my ${element.degree.type} degree in 
+            ${element.degree.program} from ${element.degree.school} in ${element.degree.year}`+ "<br />"
+          } ;
         })
 
         
