bd7c1975284baa37f1d4ed023eeec47509b2f054
eunver <iemreunver@gmail.com>
2021-12-03 16:37:39 -0500

    forloop

--- index.js
+++ index.js
@@ -11,10 +11,13 @@ async function getDegrees(url) {
       //get your data here, and check for the response status. If it's not 200, throw an error
       .then((response) => response.json())
       .then((data) =>
-        document.getElementById("degrees").innerHTML=
-          `My first degree was a ${data.data[0].degree.type} degree in 
-          ${data.data[0].degree.program} from ${data.data[0].degree.school} in ${data.data[0].degree.year}`
+        data.data.degree.forEach((element) => {
+          document.getElementById("degrees").innerHTML=
+          `My first degree was a ${element.type} degree in 
+          ${elementprogram} from ${element.school} in ${element.year}`;
+        })
+
         
-      );
+        );
   }
 
