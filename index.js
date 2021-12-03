d87eb0343a0823707479816723f6a5ceb6b3c3c5
eunver <iemreunver@gmail.com>
2021-12-03 16:05:45 -0500

    forloop2

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ async function getDegrees(url) {
     await fetch(url)
       //get your data here, and check for the response status. If it's not 200, throw an error
       .then((response) => response.json())
-      .then(data.data.forEach(element) =>
+      .then(data.data.forEach((element) =>{
         
         document.getElementById("degrees").innerHTML=
           `My first degree was a ${element.degree.type} degree
            in ${element.degree.program} from ${element.degree.school} in ${element.degree.year}`
         
-      );
+      }));
   }
   
