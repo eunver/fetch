705675c92a877c806e3a9c243b391cee24677348
eunver <iemreunver@gmail.com>
2021-12-03 12:33:45 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+async function getDegrees(url) {
+    // fetch the url
+    await fetch(url)
+      //get your data here, and check for the response status. If it's not 200, throw an error
+      .then((response) => response.json())
+      .then((data) =>
+        document.write(
+          `My first degree was a ${data.data[0].degree.school} from ${data.data[0].degree.program} in ${data.data[0].degree.type}`
+        )
+      );
+  }
+  
+  getDegrees("my_degrees.json");
