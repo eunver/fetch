db27f533a749a216f96da720209368be82cf07c6
eunver <iemreunver@gmail.com>
2021-12-03 12:58:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ async function getDegrees(url) {
       //get your data here, and check for the response status. If it's not 200, throw an error
       .then((response) => response.json())
       .then((data) =>
-        document.write(
+        document.getElementById("degrees").innerHTML=
           `My first degree was a ${data.data[0].degree.school} from ${data.data[0].degree.program} in ${data.data[0].degree.type}`
         )
       );
