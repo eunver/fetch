b0770412403ed57fe11ab43a874a028e1f49f3af
eunver <iemreunver@gmail.com>
2021-12-06 14:15:45 -0500

    script

--- index.js
+++ index.js
@@ -2,22 +2,19 @@
     var submitbutton = document.getElementById("submit");
     submitbutton.onclick = getDegrees;}
 
-
-
 async function getDegrees(url) {
-     var url="my_degrees.json";
-    // fetch the url
+     var url="my_degrees.json";   //url for json
     await fetch(url)
-      //get your data here, and check for the response status. If it's not 200, throw an error
+    
       .then((response) => response.json())
       .then((data) =>
         data.data.forEach((element) => {
-          if(element.degree.year<=2021) {
+          if(element.degree.year<=2021) {   //past tense used fo degrees gained before 2021 
           document.getElementById("degrees").innerHTML+=
           `I recieved my ${element.degree.type} degree in 
           ${element.degree.program} from ${element.degree.school} in ${element.degree.year}`+ "<br />"}
           else{
-            document.getElementById("degrees").innerHTML+=
+            document.getElementById("degrees").innerHTML+=  // future tense used for unobtained degrees
             `I will recieve my ${element.degree.type} degree in 
             ${element.degree.program} from ${element.degree.school} in ${element.degree.year}`+ "<br />"
           } ;
